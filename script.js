8ae35b78b8590517263db987dcfa9f41c786ccb5
Michaela Wagner <mickywagner@gmail.com>
2020-03-22 19:59:53 -0600

    Add validation method and error message

---
+++ script.js
@@ -0,0 +1,67 @@
+document.addEventListener(
+    "blur",
+    e => {
+      const error = hasError(e.target);
+
+      if(error) {
+        showError(e.target, error)
+        return
+      } else {
+        removeError(error)
+      }
+      
+    },
+    true
+  );
+
+  function showError(field, error) {
+    const id = field.id || field.name
+    if(!id) return
+
+    let message = field.form.querySelector('.error-message#error-for-' + id)
+    
+    if(message) {
+      return
+    } else {
+    const message = document.createElement('div')
+    message.innerHTML = error
+    message.setAttribute('class', 'error-message')
+    message.setAttribute('id', 'error-for-' + id)
+    field.classList.add('error')
+    
+    field.parentNode.insertBefore(message, field.nextSibling)
+    }
+    
+
+  }
+
+  function removeError(field) {
+    field.classList.remove('error')
+
+  }
+
+  function hasError(field) {
+    const validity = field.validity;
+
+    if (validity.valid) return
+
+    if (validity.typeMismatch) return "Please enter a valid email address.";
+
+    if (validity.patternMismatch) {
+      if (field.id === "password") {
+        const hidden = document.querySelector(".active");
+        hidden.classList.remove("hidden");
+
+        const password = field.value;
+        let confirmPassword = document.querySelector("#confirmpassword");
+        confirmPassword.setAttribute("pattern", password);
+      
+      }
+
+      if (field.id === "confirmpassword") {
+        return 'Password does not match'
+      }
+
+      return "Please use the requested format";
+    }
+  }
